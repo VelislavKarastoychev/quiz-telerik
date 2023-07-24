2e96bcaf1bf1971d1b46425c3182643190e4fc7a
VelislavKarastoychev <exel_mmm@abv.bg>
2023-07-24 17:32:03 +0300

    package.json fixed and index loaded

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-"use strick";
-
-
+"use strict";
+import message from "@euriklis/message";
+import inquirer from "inquirer";
+new message()
+    .set_color_violet()
+    .bold().italic().underline()
+    .append("Hello this is a quiz for javascript developers.")
+    .reset().log();
+// inquirer.prompt()
