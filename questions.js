0e82f212235b1c9b6d5218251ce890b8c9c39023
vasilena <vasilena.toneva@gmail.com>
2023-07-24 16:56:00 +0300

    questions

---
+++ questions.js
@@ -0,0 +1,109 @@
+ export const questions = [
+    {
+      question: "What is an object in JavaScript?",
+      type: "string",
+      answers: [
+        "An object is a data structure that stores key-value pairs.",
+        "Objects are used to represent entities with properties and behaviors.",
+        "Objects are created using curly braces {}."
+      ],
+      correctResult: 0
+    },
+    {
+      question: "What is an array in JavaScript?",
+      type: "string",
+      answers: [
+        "An array is an ordered collection of elements.",
+        "Arrays are used to store multiple values in a single variable.",
+        "Arrays are created using square brackets []."
+      ],
+      correctResult: 0
+    },
+    {
+      question: "What is a function in JavaScript?",
+      type: "string",
+      answers: [
+        "A function is a reusable block of code that performs a specific task.",
+        "Functions allow you to avoid duplicating code and make it more organized.",
+        "Functions are defined using the 'function' keyword."
+      ],
+      correctResult: 0
+    },
+    {
+      question: "What is Object-Oriented Programming (OOP) in JavaScript?",
+      type: "string",
+      answers: [
+        "OOP is a programming paradigm that focuses on organizing code into objects.",
+        "In JavaScript, objects are created using constructor functions or classes.",
+        "OOP provides concepts like inheritance and encapsulation."
+      ],
+      correctResult: 0
+    },
+    {
+      question: "What are different types of loops in JavaScript?",
+      type: "string",
+      answers: [
+        "for loop",
+        "while loop",
+        "do-while loop",
+        "for...in loop",
+        "for...of loop"
+      ],
+      correctResult: 0
+    },
+    {
+      question: "What is ESLint in JavaScript development?",
+      type: "string",
+      answers: [
+        "ESLint is a tool for identifying and reporting code style issues in JavaScript.",
+        "It helps maintain consistent coding standards and improve code quality.",
+        "ESLint configurations can be customized to match project requirements."
+      ],
+      correctResult: 0
+    },
+    {
+      question: "What is npm in JavaScript?",
+      type: "string",
+      answers: [
+        "npm is the Node Package Manager used for installing and managing packages.",
+        "It allows developers to reuse code written by others in their projects.",
+        "npm provides a vast ecosystem of open-source libraries and tools."
+      ],
+      correctResult: 0
+    },
+    {
+      question: "What are the basic data types in JavaScript?",
+      type: "string",
+      answers: [
+        "String",
+        "Number",
+        "Boolean",
+        "Null",
+        "Undefined",
+        "Symbol (new in ES6)",
+        "Object"
+      ],
+      correctResult: 0
+    },
+    {
+      question: "Code question: Write a function to add two numbers.",
+      type: "code",
+      answers: [
+        "function addNumbers(a, b) { return a + b; }",
+        "const addNumbers = (a, b) => a + b;",
+        "function addNumbers(a, b) { console.log(a + b); }"
+      ],
+      correctResult: 1
+    },
+    {
+      question: "Code question: How to check if an array includes a specific element?",
+      type: "code",
+      answers: [
+        "array.includes(element);",
+        "array.indexOf(element) > -1;",
+        "array.contains(element);"
+      ],
+      correctResult: 0
+    }
+  ];
+  
